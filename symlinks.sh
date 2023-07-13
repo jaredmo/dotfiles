74e716fc6eb88aa24d89157d41b22a6ba9172f32
Jared Monger <github@monger.cc>
2023-07-13 08:27:34 -0500

    added symlinks.sh and konsole

---
+++ symlinks.sh
@@ -0,0 +1,5 @@
+ln -sf $HOME/Projects/dotfiles/konsole/GruvboxDark.colorscheme /usr/share/konsole
+ln -sf $HOME/Projects/dotfiles/nvim $HOME/.config
+ln -sf $HOME/Projects/dotfiles/systemd/unmountnas.service /etc/systemd/system/
+ln -sf $HOME/Projects/dotfiles/tmux/.tmux.conf $HOME/
+ln -sf $HOME/Projects/dotfiles/vim/.vimrc $HOME/
