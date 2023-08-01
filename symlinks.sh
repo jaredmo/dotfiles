90273a6ca7384d39c4afcbfd46878adeee681ed3
Jared Monger <github@monger.cc>
2023-08-01 09:17:45 -0500

    updated symlink tmux

--- symlinks.sh
+++ symlinks.sh
@@ -1,6 +1,6 @@
 ln -sf $HOME/Projects/dotfiles/konsole/GruvboxDark.colorscheme /usr/share/konsole
 ln -sf $HOME/Projects/dotfiles/nvim $HOME/.config
 ln -sf $HOME/Projects/dotfiles/systemd/unmountnas.service /etc/systemd/system/
-ln -sf $HOME/Projects/dotfiles/tmux/.tmux.conf $HOME/
+ln -sf $HOME/Projects/dotfiles/tmux/wayland/.tmux.conf $HOME/
 ln -sf $HOME/Projects/dotfiles/vim/.vimrc $HOME/
 ln -sf $HOME/Projects/dotfiles/zsh/debian/.zshrc $HOME/
