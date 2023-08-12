5ceac5d8437ce5d1c97c86c4e0a70c071986d11e
Jared Monger <github@monger.cc>
2023-08-12 16:00:07 -0500

    added nvim-treesitter-context

--- nvim/lua/user/packer.lua
+++ nvim/lua/user/packer.lua
@@ -14,6 +14,7 @@ return require('packer').startup(function(use)
   }
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/nvim-treesitter-context')
   use('nvim-treesitter/playground')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
