2834af2c79c512262bb4db810813d659b2f48b4f
Jared Monger <github@monger.cc>
2024-02-07 08:09:56 -0600

    Updated lazy packages

--- nvim/lua/user/lazy.lua
+++ nvim/lua/user/lazy.lua
@@ -48,7 +48,7 @@ require("lazy").setup({
     { 'hrsh7th/cmp-nvim-lsp' },
     { 'hrsh7th/nvim-cmp' },
     { 'L3MON4D3/LuaSnip' },
-    { 
+    {
         'windwp/nvim-autopairs',
         event = "InsertEnter",
         opts = {} -- this is equalent to setup({}) function
