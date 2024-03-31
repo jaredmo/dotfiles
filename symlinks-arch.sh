eec85ecb1bf1813e16b4cf9ae50a70d51c2620ad
Jared Monger <github@monger.cc>
2024-03-31 08:16:35 -0500

    symlinks-arch updates

--- symlinks-arch.sh
+++ symlinks-arch.sh
@@ -1,9 +1,10 @@
+mkdir -p $HOME/.config/alacritty
 ln -sf $HOME/Projects/dotfiles/alacritty/arch/alacritty.toml $HOME/.config/alacritty/
-ln -sf $HOME/Projects/dotfiles/konsole/GruvboxDark.colorscheme /usr/share/konsole/
+#ln -sf $HOME/Projects/dotfiles/konsole/GruvboxDark.colorscheme /usr/share/konsole/
 ln -sf $HOME/Projects/dotfiles/nvim $HOME/.config
 ln -sf $HOME/Projects/dotfiles/radio-cli $HOME/.config
-ln -sf $HOME/Projects/dotfiles/systemd/unmountnas.service /etc/systemd/system/
-ln -sf $HOME/Projects/dotfiles/tmux/wayland/.tmux.conf $HOME/
+sudo ln -sf $HOME/Projects/dotfiles/systemd/unmountnas.service /etc/systemd/system/
+ln -sf $HOME/Projects/dotfiles/tmux/alacritty/wayland/.tmux.conf $HOME/
 ln -sf $HOME/Projects/dotfiles/vim/arch/.vimrc $HOME/
 ln -sf $HOME/Projects/dotfiles/zsh/arch/.zshrc $HOME/
 
