038789c17ce0f518c3ed24eb4cfcdc149f8dd790
Jared Monger <github@monger.cc>
2024-10-21 07:52:30 -0500

    nvimtree adaptive_size

--- nvim/after/plugin/nvimtree.lua
+++ nvim/after/plugin/nvimtree.lua
@@ -12,7 +12,7 @@ vim.opt.termguicolors = true
 require("nvim-tree").setup({
     sort_by = "case_sensitive",
     view = {
-        width = 30,
+        adaptive_size = true
     },
     renderer = {
         group_empty = true,
