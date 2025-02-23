6be35f1781fbf3cdcb6e19683fe8761af11acbd0
Jared Monger <github@monger.cc>
2025-02-23 17:20:31 -0600

    Move to waybar

--- sway/.config/sway/status.sh
+++
@@ -1,5 +0,0 @@
-battery_status=$(cat /sys/class/power_supply/BATT/status)
-battery_level=$(cat /sys/class/power_supply/BATT/capacity)
-date_formatted=$(date +'%Y-%m-%d %X')
-
-echo $battery_status $battery_level'%' $date_formatted
